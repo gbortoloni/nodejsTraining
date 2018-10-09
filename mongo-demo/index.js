a56e25394cd42e6dd519b53802f1e4cee5c638e6
Giuliano Bortoloni <giulianoagria@hotmail.com>
2018-10-09 11:05:03 -0300

    implementando queryng

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -17,9 +17,9 @@ const Course = mongoose.model('Course', couseSchema);
 
 async function createCourse() {
     const course = new Course({
-        name: 'Angular Course',
+        name: 'NodeJs Course',
         author: 'giuliano',
-        tags: ['angular', 'frontend'],
+        tags: ['node', 'frontend'],
         isPublished: true
     });
     
@@ -27,4 +27,49 @@ async function createCourse() {
     console.log(result);
 }
 
-createCourse();
+async function getCourses() {
+//eq (equal)
+//ne (not equal)
+//gt (greater than)
+//gte (greater than or equal to)
+//lt (less than)
+//lte (less than or equal to)
+//in
+//nin (not in)
+
+    const pageNumber = 2;
+    const pageSize = 10;
+    const courses = await Course
+
+
+    //Comparison Query Operators
+    //.find({ price: { $gte: 10, $lte: 20} })
+    //.find({ price: { $in: [10,15,20] } })
+
+    //Logical Query Operators
+    //.find()
+    //.or([ { author: 'giuliano' }, { isPublished: true } ])
+    //.and([])
+
+    //Regular Expression
+    //Starts with giuliano
+    //.find({ author: /^giuliano/})
+
+    //Ends with bortoloni
+    //.find({ author: /bortoloni$/i /*i case insensitive*/})
+
+    //Contains giuliano
+    //.find({ author: /.*giuliano.*/i /*case insensitve*/})
+
+    .find({ author: 'giuliano', isPublished: true})
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize)
+    .sort({name: 1})
+    .select({name: 1, tags: 1});
+    //Count
+    //.count()
+    console.log(courses);
+    
+}
+
+getCourses();
