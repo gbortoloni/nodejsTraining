f7cf02739fbc296b56bfc0bfac46df2dad393bb4
Giuliano Bortoloni <giulianoagria@hotmail.com>
2018-10-16 22:28:50 -0300

    criando CRUD no modulo genre

--- vidly/index.js
+++ vidly/index.js
@@ -1,7 +1,12 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
 const home = require('./routes/home');
+
+mongoose.connect('mongodb://localhost/vidly', { useNewUrlParser: true })
+    .then( () => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
 
 app.use(express.json());
 app.use('/api/genres', genres);
