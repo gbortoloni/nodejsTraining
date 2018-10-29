b3f6fd9ab2053c74c1d00fbd4b54561c20977614
Giuliano Bortoloni <giulianoagria@hotmail.com>
2018-10-29 10:10:35 -0300

    implementando get usuarios

--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -4,6 +4,11 @@ const {User, validate} = require('../models/users');
 const express = require('express');
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    const users = await User.find().sort('name');
+    res.send(users);
+ });
+
 router.post('/', async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
